88a74c1ca6f3f621b9b8d47807c839180c8f8a83
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-19 13:43:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function getFormvalue() {
-    //Write your code here
 
+
+function getFormvalue(e) {
+	// e.preventDefault();
+    //Write your code here
+	let fname = document.querySelector('input[name=fname]').value;
+	let lname= document.querySelector('input[name=lname]').value;
+
+	alert(`${fname} ${lname}`)
+
+	// fname="";
+	// lname="";
+
+	
 }
